d7b6dfa8f28395a7e27a9bea6db8ee697e194891
liuyunsong <yunsongliu@kaifa.cn>
2019-12-12 15:41:31 +0800

    优化dockerpush脚本

--- docker/push_image.py
+++ docker/push_image.py
@@ -7,14 +7,27 @@ import docker
 
 warnings.filterwarnings("ignore")
 
-repo_ip = '10.32.233.112'
+repo_ip = 'image.kaifa-empower.com'
 
 f1 = open('docker_images.txt','r')
 docker_images = f1.readlines()
 client = docker.from_env()
-for image in docker_images:
+save_name_list = []
+for image_name in docker_images:
+    real_name = image_name.strip().split('/',1)[1]
+    image = client.images.get(image_name.strip())
+    image.tag(repo_ip + '/'+real_name)
+    save_name_list.append( repo_ip + '/'+real_name)
+for image in save_name_list:
     print('----push '+image+'start ----')
     client.images.push(image.strip())
-    print('----push '+image+'end   ----')
-    client.images.remove(image.strip())
+
+
+for image_name in docker_images:
+    client.images.remove(image_name.strip())
+
+for real_name in save_name_list:
+    client.images.remove(real_name)
+
+
 f1.close()
