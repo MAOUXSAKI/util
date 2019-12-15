99b7cf734bcea26aaad23208ca1ed6116ee1da6c
= <YunSongLiu@kaifa.cn>
2019-12-15 13:12:27 +0800

    修改init dockerfile文件

--- openshift deploy/init.py
+++ openshift deploy/init.py
@@ -3,6 +3,23 @@ import yaml
 import shutil
 import os
 
+environment_config_list = {
+    "DEV": "http://10.32.233.112:8080",
+    "QA": "http://10.32.233.112:8081",
+    "UAT": "http://10.32.233.112:8082",
+    "CLOUD": "http://10.0.0.20:8080"
+}
+
+environment_ip_list = {
+    "DEV": "http://10.32.233.12",
+    "QA": "http://10.32.233.31"
+}
+
+environment_url_list = {
+    "DEV": ".kaifa.develop",
+    "QA": ".kaifa.tst"
+}
+
 
 def mkdir(path):
     path = path.strip()
@@ -13,6 +30,7 @@ def mkdir(path):
         return True
     else:
         return False
+
 
 def remove_dir(path):
     isExists = os.path.exists(path)
@@ -69,6 +87,7 @@ def replace(file_path, old_str, new_str):
 class Service:
     def __init__(self):
         self.repository = ""
+        self.code = ""
         self.name = ""
         self.tag = ""
         self.image = ""
@@ -81,12 +100,14 @@ class Service:
         self.config = ""
         self.volumes = []
         self.namespace = ""
+        self.apollo_meta = ""
 
-    def set_info(self, name, data):
-        if name != "web":
-            self.name = "api-" + name + "-service"
+    def set_info(self, code, data):
+        self.code = code
+        if code != "web":
+            self.name = "api-" + code + "-service"
         else:
-            self.name = name
+            self.name = code
         if "port" in data:
             self.port = data["port"]
         if "config-dir" in data:
@@ -104,25 +125,33 @@ class Service:
             self.volumes = data['volumes']
 
     def generate_openshift_file(self):
-        if self.name == "web":
-            shutil.copyfile("template/web_template.yml", "target/ami-web.yml")
+        target_file_name = "target/ami-" + self.name + ".yml"
+        service_file_name = "target/service-" + self.name + ".yml"
+        if self.port != "":
+            shutil.copyfile("template/service_template.yml", service_file_name)
+            replace(service_file_name,"@PORT@",str(self.port))
+            replace(service_file_name, "@NAME@", self.name)
+        if self.code == "web":
+            shutil.copyfile("template/web_template.yml",target_file_name)
             replace("target/ami-web.yml", "@IMAGE@", self.image)
             replace("target/ami-web.yml", "@TIME_ZONE@", self.time_zone)
         else:
-            if self.name == "config":
-                target_file_name = "target/ami-config.yml"
+            if self.code == "config":
                 shutil.copyfile("template/config_template.yml", target_file_name)
             else:
-                target_file_name = "target/ami-" + self.name + ".yml"
                 shutil.copyfile("template/template.yml", target_file_name)
+            if self.code == "gateway":
+                replace("target/web-config-map.yml", "@PORT@", str(self.port))
             replace(target_file_name, "@IMAGE@", self.image)
             replace(target_file_name, "@MEMORY@", self.memory)
             replace(target_file_name, "@NAME@", self.name)
             replace(target_file_name, "@TIME_ZONE@", self.time_zone)
             replace(target_file_name, "@CONFIG_MODE@", self.config_mode)
             replace(target_file_name, "@PROJECT@", self.project_template)
-            replace(target_file_name, "@APOLLO_PROJECT@", self.project)
-            replace(target_file_name, "@NAMESPACE@",self.namespace)
+            replace(target_file_name, "@APOLLO_PROJECT@", "ami")
+            replace(target_file_name, "@NAMESPACE@", self.namespace)
+            replace(target_file_name, "@PROJECT_CLUSTER@", self.project)
+            replace(target_file_name, "@APOLLO_META@", self.apollo_meta)
 
 
 class Project:
@@ -134,6 +163,8 @@ class Project:
             self.service_list = []
             self.config_mode = ""
             self.namespace = ""
+            self.apollo_meta = ""
+            self.env = ""
         else:
             self.project = config['project']
             if 'project_template' in config:
@@ -152,24 +183,38 @@ class Project:
                 self.namespace = config['namespace']
             else:
                 self.namespace = "infrastructure,dateformat,external-api,web-other"
+            if 'env' in config:
+                self.env = config['env']
+            else:
+                self.env = "DEV"
+
+            if 'apollo_meta' in config:
+                self.apollo_meta = config['apollo_meta']
+            else:
+                self.apollo_meta = environment_config_list[self.env]
+
             self.time_zone = config['tz']
             self.web = config['web']
             self.service_list = []
-            for k,v in self.web['services'].items():
+            for k, v in self.web['services'].items():
                 service = Service()
+                service.apollo_meta = self.apollo_meta
                 service.repository = self.repository
                 service.project = self.project
                 service.project_template = self.project_template
                 service.time_zone = self.time_zone
                 service.config_mode = self.config_mode
                 service.namespace = self.namespace
-                service.set_info(k,v)
+                service.set_info(k, v)
                 self.service_list.append(service)
 
     def generate_openshift_file(self):
         remove_dir("target")
         mkdir("target")
         shutil.copyfile("template/web-config-map.yml", "target/web-config-map.yml")
+        shutil.copyfile("template/web-router.yml", "target/web-router.yml")
+        replace("target/web-config-map.yml","@ENVIRONMENT_IP@",environment_ip_list[self.env])
+        replace("target/web-router.yml","@HOST@",self.project + environment_url_list[self.env])
         shutil.copyfile("template/service.yml", "target/service.yml")
         for service in self.service_list:
             service.generate_openshift_file()
