bf05084f8361f685c0909323477cc2128a1cfae7
= <YunSongLiu@kaifa.cn>
2019-12-22 14:22:55 +0800

    更新base_tool

--- base_tool/excel_tool.py
+++ base_tool/excel_tool.py
@@ -30,7 +30,7 @@ def set_row_align(worksheet, row, num, style):
         worksheet.cell(row, i).alignment = style
 
 
-def get_attr_list(worksheet, num=None):
+def get_attr_list(worksheet, num=None,clear_comment=False):
     attr_list = []
     flag = False
     if num == None:
@@ -39,6 +39,8 @@ def get_attr_list(worksheet, num=None):
     for i in range(1, num + 1):
         cell = worksheet.cell(1, i)
         text = cell.comment.text.strip('''\n''')
+        if clear_comment:
+            cell.comment.text = ''
         if flag:
             if text.endswith(';'):
                 text = text.rstrip(';')
@@ -53,26 +55,27 @@ def set_sheet_data(worksheet, data_list, style=None, attr_list=None, num=None):
     if style is None:
         style = get_template_style(worksheet)
     if attr_list is None:
-        attr_list = get_attr_list(worksheet, num)
+        attr_list = get_attr_list(worksheet, num,clear_comment=True)
     for i in range(len(data_list)):
         print(i)
         for j in range(len(attr_list)):
+            cell = worksheet.cell(i + 2, j + 1)
             if attr_list[j] == 'no':
-                worksheet.cell(i + 2, j + 1, i + 1)
+                cell.value = i + 1
             else:
                 value = getattr(data_list[i], attr_list[j])
                 if value is not None:
                     if isinstance(value,str):
                         value = value.strip('=').strip(' ')
-                    worksheet.cell(i + 2, j + 1, value)
+                    cell.value = value
             if isinstance(style,list):
                 style_data = style[j]
             else:
                 style_data = style
-            worksheet.cell(i + 2, j + 1).border = style_data.border
-            worksheet.cell(i + 2, j + 1).font = style_data.font
-            worksheet.cell(i + 2, j + 1).alignment = style_data.alignment
-            worksheet.cell(i + 2, j + 1).number_format = style_data.number_format
+            cell.border = style_data.border
+            cell.font = style_data.font
+            cell.alignment = style_data.alignment
+            cell.number_format = style_data.number_format
 
 
 def get_sheet_data(worksheet, clazz=ExcelData, attr_list=None):
